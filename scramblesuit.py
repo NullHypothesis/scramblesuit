350e790f0adc9974ffca51cabe6d6bf3f13d5171
Philipp Winter <identity.function@gmail.com>
2013-02-10 12:59:46 +0100

    Move packet morpher to packetmorpher.py.

--- scramblesuit.py
+++ scramblesuit.py
@@ -28,6 +28,7 @@ import timelock
 import mycrypto
 import message
 import const
+import packetmorpher
 
 
 
@@ -43,45 +44,6 @@ def ntohs( data ):
 
 def htons( data ):
 	return struct.pack('!H', data)
-
-
-
-class PacketMorpher( object ):
-	"""Provides an interface to morph large chunks of bytes to a given target
-	probability distribution. This is implemented by naively sampling the
-	target probability distribution."""
-
-	def __init__( self, dist=None ):
-		"""Initialize the PacketMorpher with a discrete probability
-		distribution. If none is given, a distribution is randomly
-		generated."""
-
-		self.dist = dist if dist else \
-			probdist.RandProbDist(lambda: random.randint(1, const.MTU))
-
-
-	def morph( self, dataLen ):
-		"""Based on `dataLen', the length of the data to morph, this function
-		returns a chopper function which is used to chop the data as well as
-		the length of padding which is appended to the last protocol
-		message."""
-
-		breakPoints = []
-		lastBreakPoint = 0
-		progress = 0
-
-		while progress < dataLen:
-			newBreakPoint = progress + self.dist.randomSample()
-			breakPoints.append((lastBreakPoint, newBreakPoint))
-			lastBreakPoint = newBreakPoint
-			progress += newBreakPoint
-
-		paddingLen = progress - dataLen
-		breakPoints.append((lastBreakPoint, progress))
-
-		chopper = lambda data: [data[x:y] for (x, y) in breakPoints]
-
-		return (chopper, paddingLen)
 
 
 
@@ -221,7 +183,7 @@ class ScrambleSuitDaemon( base.BaseTransport ):
 		log.debug("Local HMAC key:  %s" % self.sendHMAC.encode('hex'))
 		log.debug("Remote HMAC key: %s" % self.recvHMAC.encode('hex'))
 
-		self.pktMorpher =  PacketMorpher()
+		self.pktMorpher =  packetmorpher.PacketMorpher()
 
 		log.debug("Magic values derived from session key: send=0x%s, " \
 			"recv=0x%s." % (self.sendMagic.encode('hex'), \
