c6dd52df08f3be35212e5ab3f9fec81ced24c8c1
Philipp Winter <phw@torproject.org>
2013-09-18 05:53:21 +0200

    Remove the `morph()' method because it is no longer used.

--- packetmorpher.py
+++ packetmorpher.py
@@ -48,30 +48,6 @@ class PacketMorpher( object ):
 
         return padLen
 
-    def morph( self, dataLen ):
-        """Based on `dataLen', the length of the data to morph, this function
-        returns a chopper function which is used to chop the data as well as
-        the length of padding which is appended to the last protocol
-        message."""
-
-        breakPoints = []
-        lastBreakPoint = 0
-        progress = 0
-
-        while progress < dataLen:
-            newBreakPoint = progress + self.dist.randomSample()
-            breakPoints.append((lastBreakPoint, newBreakPoint))
-            lastBreakPoint = newBreakPoint
-            progress = newBreakPoint
-
-        paddingLen = progress - dataLen
-        breakPoints.append((lastBreakPoint, progress))
-
-        chopper = lambda data: [data[x:y] for (x, y) in breakPoints]
-
-        return (chopper, paddingLen)
-
-
     def randomSample( self ):
         """Return a random sample of the stored probability distribution."""
 
