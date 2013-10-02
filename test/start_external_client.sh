f7cdbfc59bb2fcc57dd82a1436e103dfc4999ae2
Philipp Winter <phw@torproject.org>
2013-10-02 13:38:01 +0200

    Add scripts to start ScrambleSuit in external mode.

---
+++ test/start_external_client.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# This command starts an obfsproxy instance which listens for SOCKS connections
+# on localhost:50001.  Incoming SOCKS data is then forwarded to the
+# ScrambleSuit server running on localhost:50000.  Persistent data (the
+# client's session ticket) is stored in /tmp/scramblesuit-client.
+
+python /usr/local/bin/obfsproxy \
+	--log-min-severity=debug \
+	--data-dir=/tmp/scramblesuit-client \
+	scramblesuit \
+	--shared-secret=BANANASAPPLESCOCONUTSPEACHESEGGS  \
+	--dest 127.0.0.1:50000 \
+	client 127.0.0.1:50001
---
+++ test/start_external_server.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# This command starts an obfsproxy instance which is listening on
+# localhost:50000 for incoming ScrambleSuit connections.  The incoming data is
+# then deobfuscated and forwarded to localhost:1234 which could run a simple
+# echo service.  Persistent data (the server's state) is stored in
+# /tmp/scramblesuit-server.
+
+python /usr/local/bin/obfsproxy \
+	--log-min-severity=debug \
+	--data-dir=/tmp/scramblesuit-server \
+	scramblesuit \
+	--shared-secret=BANANASAPPLESCOCONUTSPEACHESEGGS \
+	--dest 127.0.0.1:1234 \
+	server 127.0.0.1:50000
